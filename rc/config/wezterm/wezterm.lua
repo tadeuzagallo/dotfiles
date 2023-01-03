c2f51bc885135b8541dfa2534ff217b7df183357
Tadeu Zagallo <tadeuzagallo@gmail.com>
2023-01-03 14:53:47 +0100

    Add basic WezTerm config

---
+++ rc/config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+	color_scheme = 'Dracula (Official)',
+	font = wezterm.font 'Hasklig',
+	tab_bar_at_bottom = true,
+	use_fancy_tab_bar = false,
+	window_decorations = 'RESIZE',
+}
