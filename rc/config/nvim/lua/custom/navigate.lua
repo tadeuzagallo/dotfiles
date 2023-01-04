e6bd66d6e5a5e55cae04ee2231319f4de479bf7a
Tadeu Zagallo <tadeuzagallo@gmail.com>
2023-01-04 16:38:00 +0100

    Remove tmux integration

--- rc/config/nvim/lua/custom/navigate.lua
+++ rc/config/nvim/lua/custom/navigate.lua
@@ -14,17 +14,23 @@ local function navigate(direction)
       vim.cmd('tabnext')
     end
 
-    if nr == fn.tabpagenr() then
-      local tmuxDirection = fn.tr(direction, 'phjkl', 'lLDUR')
-      fn.system { 'tmux', 'select-pane', '-' .. tmuxDirection }
-    end
+    --if nr == fn.tabpagenr() then
+      --local directionMap = {
+        --h = 'Left',
+        --j = 'Down',
+        --k = 'Up',
+        --l = 'Right',
+      --}
+      --local termDirection = directionMap[direction]
+      --fn.system { '/Applications/WezTerm.app/Contents/MacOS/wezterm', 'cli', 'activate-pane-direction', termDirection }
+    --end
   end
 end
 
 -- map('n', '<C-l><C-l>', '<cmd>lua require("custom.navigate")("p")<cr>') 
-map('n', '<C-h>', '<cmd>lua require("custom.navigate")("h")<cr>') 
-map('n', '<C-j>', '<cmd>lua require("custom.navigate")("j")<cr>') 
-map('n', '<C-k>', '<cmd>lua require("custom.navigate")("k")<cr>') 
-map('n', '<C-l>', '<cmd>lua require("custom.navigate")("l")<cr>') 
+map('n', '<C-h>', '<cmd>lua require("custom.navigate")("h")<cr>')
+map('n', '<C-j>', '<cmd>lua require("custom.navigate")("j")<cr>')
+map('n', '<C-k>', '<cmd>lua require("custom.navigate")("k")<cr>')
+map('n', '<C-l>', '<cmd>lua require("custom.navigate")("l")<cr>')
 
 return navigate
